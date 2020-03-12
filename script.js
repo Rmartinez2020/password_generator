e2590ecb10002aed760712e523a4aa6682b6fbb2
Roger <rmartinez5926.rm@gmail.com>
2020-03-12 19:25:53 -0400

    added global variables and variables to hold user choices

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//declare arrays with usable characters
+
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var lettersLower = "abcdefghijklmnopqrstuvwxyz";
+var lettersUpper = "ABCDEFGHIJKLMNOPQRSTUBWXYZ";
+var specialChar = [".-_!@#$%^&*+"];
+
+
+function generatePassword() {
+  //ask the user what kind of characters they want in the password and then logs the response
+  var numChar = prompt("How many characters would you like to use?");
+  console.log(numChar);
+  var useLower = confirm("Would you like lowercase letters?");
+  console.log(useLower);
+  var useUpper = confirm("Would you like uppercase letters?");
+  console.log(useUpper);
+  var useSpecial = confirm("Would you like speacial characters");
+  console.log(useSpecial);
+  var useNumbers = confirm("Would you like numbers?");
+  console.log(useNumbers);
+
+
+  return "THIS IS THE PASSWORD"
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
