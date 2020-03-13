46620c1156b47254c7d4e47decabe3349ff28db9
Roger <rmartinez5926.rm@gmail.com>
2020-03-13 00:32:20 -0400

    creat array to pull values for password

--- script.js
+++ script.js
@@ -1,26 +1,55 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//declare arrays with usable characters
+//declare variables with usable characters
 
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var numbers = "0123456789";
 var lettersLower = "abcdefghijklmnopqrstuvwxyz";
 var lettersUpper = "ABCDEFGHIJKLMNOPQRSTUBWXYZ";
-var specialChar = [".-_!@#$%^&*+"];
+var specialChar = ".-_!@#$%^&*+";
 
 
 function generatePassword() {
-  //ask the user what kind of characters they want in the password and then logs the response
+  //create empty array to pull characters from
+  var possibleChar = [];
+  console.log(possibleChar);
+  //creat empty string to save values in
+  var userPass = "";
+  console.log(userPass);
+
+  //ask the user what kind of characters they want in the password and then logs the response and add them to an array
+
+
   var numChar = prompt("How many characters would you like to use?");
-  console.log(numChar);
+  //console.log(numChar);
+
   var useLower = confirm("Would you like lowercase letters?");
-  console.log(useLower);
+  // console.log(useLower);
+  //if true then add lowercase characters to the possibleChar array
+  if (useLower) {
+    possibleChar.push(lettersLower);
+  }
+
   var useUpper = confirm("Would you like uppercase letters?");
-  console.log(useUpper);
-  var useSpecial = confirm("Would you like speacial characters");
-  console.log(useSpecial);
+  // console.log(useUpper);
+  //if true then add uppercase charaters to the possibleChar array
+  if (useUpper) {
+    possibleChar.push(lettersUpper);
+  }
+
+  var useSpecial = confirm("Would you like special characters");
+  // console.log(useSpecial);
+  //if true then add special characters to the possibleChar array
+  if (useSpecial) {
+    possibleChar.push(specialChar);
+  }
+
   var useNumbers = confirm("Would you like numbers?");
-  console.log(useNumbers);
+  //console.log(useNumbers);
+  if (useNumbers) {
+    possibleChar.push(numbers)
+  }
+
 
 
   return "THIS IS THE PASSWORD"
